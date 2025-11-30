51203f944b60d825f52a4426c368f327fd0a7d2e
Parker Harlson <71110669+ParkerHarlson@users.noreply.github.com>
2025-11-29 20:02:46 -0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,8 @@ window.addEventListener("DOMContentLoaded", () => {
       scaleMobile: 1.0,
       color: 0x705cff,
       backgroundColor: 0x151515,
-      points: 7.0,
-      maxDistance: 24.0,
+      points: 4.0,
+      maxDistance: 45.0,
       spacing: 14.0,
       showDots: true, 
     });
